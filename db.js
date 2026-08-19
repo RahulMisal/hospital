7cb6b508e4402312bfab66aadc254377b02a82ea
Rahul Misal <86588411+RahulMisal@users.noreply.github.com>
2026-08-19 20:01:36 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,11 +2,10 @@ var mysql=require('mysql2');
 var util=require('util');
 
 var conn=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'root',
-    database:'hospital_web',
-    port:3307
+    host:'brtrgihkpflcpdlmwxtj-mysql.services.clever-cloud.com',
+    user:'uh1jhckgfyweaqit',
+    password:'QAvKSrKX83YEG6wExWzQ',
+    database:'brtrgihkpflcpdlmwxtj'
 });
 
 var exe=util.promisify(conn.query).bind(conn);
